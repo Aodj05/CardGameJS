508a987c6f3c1fcf1a5a6c9dc56c59d298d697dd
Aodj05 <aodj05@msn.com>
2022-08-26 12:59:06 -0400

    Audio controller

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('Assets/Audio/safari.wav');
+        this.flipSound = new Audio('Assets/Audio/flip.wav');
+        this.gameOverSound = new Audio('Assets/Audio/game-over.wav');
+        this.matchSound = new Audio('Assets/Audio/match.wav');
+        this.victorySound = new Audio('Assets/Audio/victory.wav');
+        this.bgMusic.volume = 0.5;
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
 }
 
 function ready() {
@@ -20,3 +47,9 @@ function ready() {
         });
     });
 };
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
